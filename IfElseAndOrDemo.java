b04a7cd1349085d0b071fd1592241c9283a413cf
sharifabee <sharifabee.alur@gmail.com>
2025-01-20 15:16:00 +0530

    if else demo

---
+++ IfElseAndOrDemo.java
@@ -0,0 +1,17 @@
+
+public class IfElseAndOrDemo {
+
+	public static void main(String[] args) {
+		
+		
+		int num = 10;
+		
+		
+		if( (num++ > 0) || (++num > 5) && (num++ > 8)) {// 
+			System.out.println("+ve number...");
+			//System.out.println("num --->"+num);
+		}
+		System.out.println("num--->"+num);
+	}
+
+}
